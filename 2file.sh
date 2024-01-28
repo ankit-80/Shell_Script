6d9945e0a327997485915b85446ec9db6b5c940a
simbha <ankitkate01@gmail.com>
2024-01-28 08:26:18 +0530

    2nd commit in 'script_error branch'

--- 2file.sh
+++ 2file.sh
@@ -5,10 +5,13 @@ c=$a+$b
 echo "addtion of a & b is $c"
 
 
-
+##1st commit 
  +++++++++++++++++++++++++++++++
  hello guys how are you?
 
 this is me ankit
 ok 
+##2nd commit
+echo "we also added this text in program"
+
 
